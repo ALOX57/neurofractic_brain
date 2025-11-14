3770bfe089b0dabf9cd36e154c49d916058637fb
Alexander Birtles <aloxemail@gmail.com>
2025-11-14 19:53:40 +1100

    chore: cleanup of old code

--- brain_sim/brain.py
+++ brain_sim/brain.py
@@ -38,36 +38,3 @@ class Brain:
 
     def set_sensory(self, v):
         self.sns = v
-
-
-
-    # def __init__(self, size=SIZE, offsets = OFF):
-    #     self.size = size
-    #
-    #
-    #     self.sns = [[0.0 for _ in range(size)] for _ in range (size)]
-    #     self.prd = [[0.0 for _ in range(size)] for _ in range (size)]
-    #
-    #     self.sens_hat = [[0.0 for _ in range(size)] for _ in range (size)]
-    #     self.errors = [[0.0 for _ in range(size)] for _ in range(size)]
-    #
-    #     self.connections = [[[[0.0 for _ in range(3)] for _ in range(3)] for _ in range (size)] for _ in range(size)]
-    #
-    #     for y, row in enumerate(self.prd):
-    #         for x, cell in enumerate(row):
-    #             for dy in offsets:
-    #                 for dx in offsets:
-    #                     ny, nx = y + dy, x + dx
-    #                     if 0 <= ny < size and 0 <= nx < size:
-    #                         dy_i = dy + 1
-    #                         dx_i = dx + 1
-    #                         self.connections[y][x][dy_i][dx_i] = random.uniform(-0.03, 0.03)
-    #
-    #
-    #
-    #
-    # def set_cell(self, y, x, v):
-    #         self.sns[y][x] = v
-    #
-    # def get_cell(self, y, x):
-    #     return self.sns[y][x]
--- main.py
+++ main.py
@@ -47,27 +47,5 @@ def main():
     viz.close()
 
 
-
-
-    # brain = Brain()
-    #
-    # for (y, x), v in SEEDS:
-    #     brain.set_cell(y, x, v)
-    #
-    # loop = FixedRateLoop(TICK_S)
-    #
-    # def do_step(i):
-    #     step_predictive(brain, ALPHA)
-    #
-    # wall_ms_total, avg_compute_ms, avg_wall_ms = loop.run(STEPS, do_step)
-    #
-    # print(f"Total wall: {wall_ms_total:.4f} ms")
-    # print(f"Avg compute per step: {avg_compute_ms:.4f} ms")
-    # print(f"Avg wall per step (incl. sleep): {avg_wall_ms:.4f} ms")
-    # print(f"Budget used: {avg_compute_ms / (TICK_S * 1000):.4%} of {TICK_S * 1000:.0f} ms")
-    #
-    # print(brain.sns[3][3])
-    # plot_heatmap(brain.prd)
-
 if __name__ == "__main__":
     main()
